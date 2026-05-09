a09b908f82ee31e1329916431fda8c29bbd79683
RodrigoGarciaNunez <118324022+RodrigoGarciaNunez@users.noreply.github.com>
2026-05-09 13:08:32 -0600

    Time: 7 ms (64.66%), Space: 12.3 MB (42.79%) - LeetHub

--- 0003-longest-substring-without-repeating-characters/0003-longest-substring-without-repeating-characters.cpp
+++ 0003-longest-substring-without-repeating-characters/0003-longest-substring-without-repeating-characters.cpp
@@ -5,7 +5,53 @@
 class Solution {
 public:
     int lengthOfLongestSubstring(string s) {
+        
+        int string_size = s.length();
         int longest_size=1;
+        int size=0;
+
+        const char * left = s.c_str();
+        
+        if(*left == '\0'){
+            return 0;
+        }
+
+        const char * right = left;
+
+        unordered_map<char, const char *> letters_map;
+        
+        //letters_map[*left] = left;
+
+        //cout << static_cast<const void*>(left)  << endl;
+
+        while(*right != '\0'){
+
+
+            //si la dirección está mapeada y dentro de la ventana
+            if(letters_map.find(*right) != letters_map.end() && letters_map.at(*right) >= left){ 
+                left = letters_map[*right]+1;
+            }
+
+            size = right - left +1 ;
+            longest_size = (size > longest_size) ? size : longest_size;
+            
+            letters_map[*right] = right;
+            ++right;
+        
+        }
+
+        return longest_size;
+    }
+
+                
+                
+
+
+};
+
+
+/*
+int longest_size=1;
         int size=1;
         const char * left = s.c_str();
         
@@ -44,4 +90,4 @@ public:
 
         return longest_size;
     }
-};
+*/
