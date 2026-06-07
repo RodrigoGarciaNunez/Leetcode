8209bf65ece389dcd7e76a4e56fe60a7541cadc7
RodrigoGarciaNunez <118324022+RodrigoGarciaNunez@users.noreply.github.com>
2026-06-06 18:12:51 -0600

    Time: 3 ms (81.2%), Space: 12.1 MB (43.82%) - LeetHub - Time: O(?), Space: O(?)

--- 0003-longest-substring-without-repeating-characters/0003-longest-substring-without-repeating-characters.cpp
+++ 0003-longest-substring-without-repeating-characters/0003-longest-substring-without-repeating-characters.cpp
@@ -7,35 +7,34 @@ public:
     int lengthOfLongestSubstring(string s) {
         
         int string_size = s.length();
-        int longest_size=1;
-        int size=0;
+        int longest_size=1;  //el minimo posible para un s no vació 1
+        int size=0;        //se inicializa el size en 0. Este se obtandrá con la diferencia entre cotas izquierda y derecha
 
-        const char * left = s.c_str();
+        const char * left = s.c_str(); //apuntador de solo lectura al inicio de la cadena
         
         if(*left == '\0'){
-            return 0;
+            return 0;           //si s es vació, pues se retorna 0
         }
 
-        const char * right = left;
+        const char * right = left; 
 
         unordered_map<char, const char *> letters_map;
         
-        //letters_map[*left] = left;
+        //letters_map[*left] = left; si haces esto el entrarás al primer if y en casos de cadenas de tamaño 2 con chars diferentes, el resultado será simpre 1 y no 2.
 
-        //cout << static_cast<const void*>(left)  << endl;
-
+    
         while(*right != '\0'){
 
 
-            //si la dirección está mapeada y dentro de la ventana
+            //si la dirección está mapeada y dentro de la ventana, ya hubo repetición
             if(letters_map.find(*right) != letters_map.end() && letters_map.at(*right) >= left){ 
-                left = letters_map[*right]+1;
+                left = letters_map[*right]+1;   // se acutualiza la cota izquierda a la primera aparición de la letra que se repitió    
             }
 
-            size = right - left +1 ;
+            size = right - left + 1; //se suma 1 para contar el tamaño bruto de la subcadena 
             longest_size = (size > longest_size) ? size : longest_size;
             
-            letters_map[*right] = right;
+            letters_map[*right] = right; //se actualiza de referencia de la última letra y movemos la cota derecha.
             ++right;
         
         }
@@ -91,3 +90,8 @@ int longest_size=1;
         return longest_size;
     }
 */
+
+
+// Synced seamlessly with LeetHub Pro
+// Pro features: https://bit.ly/leethubpro | Free version: https://bit.ly/leethubv4
+// Get it here: https://chromewebstore.google.com/detail/bcilpkkbokcopmabingnndookdogmbna
