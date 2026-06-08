3baaecb18365590afac117a5c05f800456c2613f
RodrigoGarciaNunez <118324022+RodrigoGarciaNunez@users.noreply.github.com>
2026-06-08 14:37:12 -0600

    Time: 0 ms (100%), Space: 94.9 MB (99.81%) - LeetHub - Time: O(?), Space: O(?)

--- 0004-median-of-two-sorted-arrays/0004-median-of-two-sorted-arrays.cpp
+++ 0004-median-of-two-sorted-arrays/0004-median-of-two-sorted-arrays.cpp
@@ -6,7 +6,6 @@ public:
         size_t total_size = nums1.size() + nums2.size();
         int middle = (total_size % 2 !=0 ) ? std::floor(total_size/2): (total_size/2) -1;
 
-        std::cerr << "middle: "<< middle << std::endl;
         
         if(nums1.size()== 0 || nums2.size()==0){
             double result=0;
@@ -41,15 +40,12 @@ public:
 
             it1_value = (it1 == nums1.end()) ? *it2+1: *it1;
             it2_value = (it2== nums2.end()) ? *it1+1: *it2;
-            std::cerr << "it1: " << it1_value << " it2: "<< it2_value <<  "\n";
 
             if (it1 != nums1.end() && it1_value < it2_value){
-                std::cerr << *it1 << "\n";
                 overall_index.second=*it1;
                 it1 = (it1 == nums1.end()) ? it1: ++it1;
             }
             else if(it2 != nums2.end()){
-                std::cerr << *it2 << "\n";
                 overall_index.second=*it2;
                 it2 = (it2 == nums2.end()) ? it2: ++it2;
             }
@@ -57,27 +53,13 @@ public:
             if(overall_index.first == middle){
                 
                 if(total_size%2 != 0){ 
-                    std::cerr << "imparpar" << std::endl;
                     return static_cast<double>(overall_index.second);
                 }
                 else{
-                    /*
-                    std::cerr << "par" << std::endl;
-                    if(it1 == nums1.end()){
-                        std::cerr << "par t1" << std::endl;
-                        return static_cast<double>((overall_index.second + *it2)/2); 
-                    }
-                    else{
-                        std::cerr << "par t2" << std::endl;
-                        return static_cast<double>((overall_index.second + *it1)/2); 
-                    }
-                    */  
 
                     it1_value = (it1 == nums1.end()) ? *it2+1: *it1;
                     it2_value = (it2== nums2.end()) ? *it1+1: *it2;
 
-                    std::cerr << "par normal" << std::endl;
-                    std::cerr << "it1: " << it1_value << " it2: "<< it2_value <<  "\n";
                     int second_mid = (it1_value < it2_value) ? it1_value : it2_value;
                     return static_cast<double>((overall_index.second + second_mid)/2);
                 }
